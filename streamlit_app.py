81ffc2b66c5933c95bae2e864b2c716b8de7f92d
marucarrillog <152593620+marucarrillog@users.noreply.github.com>
2023-12-01 09:24:43 +0100

    Update streamlit_app.py
    add S3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
@@ -8,3 +9,5 @@ st.text('🐔 Hard-Boiled Free-Range Egg 🐔')
 st.text('🥑🍞 Avocado Toast 🥑🍞')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")

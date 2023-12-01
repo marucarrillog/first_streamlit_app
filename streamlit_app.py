8bed6126757f21498805311272ff7baeb3663b95
marucarrillog <152593620+marucarrillog@users.noreply.github.com>
2023-12-01 08:56:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-#st.title('My Parents New Healthy Diner')
-#st.header('Breakfast Menu')
-#st.subheader('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
+st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.subheader('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
 
-st.title('🥑🍞Roberto Sanz Moya🥑🍞')
+#st.title('🥑🍞Roberto Sanz Moya🥑🍞')

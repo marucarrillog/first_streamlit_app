c1d4163e27715d02ab664b1e77698909c3a8716f
marucarrillog <152593620+marucarrillog@users.noreply.github.com>
2023-12-01 08:50:42 +0100

    Update streamlit_app.py
    fixing error subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
-st.subheader('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket 
-Smoothie\nHard-Boiled Free-Range Egg')
+st.subheader('Omega 3 & Blueberry Oatmeal\nKale, Spinach & Rocket Smoothie\nHard-Boiled Free-Range Egg')
 

db0480587232396b66e6ad0aa7b802a3dee7d481
marucarrillog <152593620+marucarrillog@users.noreply.github.com>
2023-12-04 16:31:35 +0100

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # represent json request as a table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
 
 
